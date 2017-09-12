949b8d26e599c1fcff38a1dc4ab46e36ced14596
gaoxz <2285687708@qq.com>
2017-09-12 17:18:09 +0800

    GUI

--- path_select.py
+++ path_select.py
@@ -6,15 +6,16 @@ import os
 
 
 def imagemodify():
-    images_path = r'F:\images\001'   #原始文件路径
+    images_path = getValue(e1)   #原始文件路径
     images = 0  #总图像数目统计
     num = 1300000  #病人计数
-    checkNo = 2017082403001   #初始检查号
+    checkNo = int(getValue(PatientName))   #初始检查号
     for root, dirs, files in os.walk(images_path):
        
         if files:
             images_cnt = 0  #单文件图像数目统计
-            filedirs = "./133h333"+str(checkNo) #新建文件路径
+            filedirs = str(getValue(e2))+"/"+str(checkNo) #新建文件路径
+            
             #print(filedirs)
             os.mkdir(filedirs)
             i =0
@@ -41,7 +42,6 @@ def imagemodify():
             print(os.path.join(root,),"图像数量：{}".format(images_cnt))
     print("图像总数量为：{}".format(images))
     tkinter.messagebox.showinfo("message","修改完成")
-    root.destroy()
 
 
 
@@ -49,22 +49,29 @@ def selectPath(path):
 	path_=askdirectory()
 	path.set(path_)
 
+def getValue(e):
+	_path=e.get()
+	print(_path)
+	return _path
+
 root = Tk()
-root.geometry('350x500+500+200')
+root.geometry('320x250+800+200')
 root.title("DICOM MODIFY")
 select_path = StringVar()
 save_path= StringVar()
 
 Label(root,text="源文件路径:").grid(row=1,column=0,sticky=W)
-Entry(root,textvariable=select_path).grid(row=1,column=1)
+e1=Entry(root,textvariable=select_path)
+e1.grid(row=1,column=1)
 Button(root,text="路径选择",command=lambda:selectPath(select_path)).grid(row=1,column=2)
-
 Label(root,text="保存路径:").grid(row=2,column=0,sticky=W)
-Entry(root,textvariable=save_path).grid(row=2,column=1)
+e2=Entry(root,textvariable=save_path)
+e2.grid(row=2,column=1)
 Button(root,text="路径选择",command=lambda:selectPath(save_path)).grid(row=2,column=2)
 
 Label(root,text="PatientName").grid(row=3,sticky=W)
-Entry(root).grid(row=3,column=1,sticky=E)
+PatientName=Entry(root)
+PatientName.grid(row=3,column=1,sticky=E)
 Label(root,text="PatientID").grid(row=4,sticky=W)
 Entry(root).grid(row=4,column=1,sticky=E)
 Label(root,text="StudyInstanceUID").grid(row=5,sticky=W)
@@ -76,11 +83,7 @@ Entry(root).grid(row=7,column=1,sticky=E)
 Label(root,text="StudyDate").grid(row=8,sticky=W)
 Entry(root).grid(row=8,column=1,sticky=E)
 
-Button(root,text="执行",command=imagemodify).grid(column=1,sticky=N)
-
-
-
-
+Button(root,text="执行",command=imagemodify).grid(row=10,column=0,sticky=E)
+Button(root,text="退出",command=root.quit).grid(row=10,column=1,sticky=E) 
 
 root.mainloop()
-root.destroy()
